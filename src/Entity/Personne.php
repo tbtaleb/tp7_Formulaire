da81f3ffa4f44eeb7500a4b692c583dfd5991590
tbtaleb* <tbfeeber@gmail.com>
2024-05-17 09:38:27 +0100

    first

---
+++ src/Entity/Personne.php
@@ -0,0 +1,36 @@
+
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonneRepository::class)]
+class Personne
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomp = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomp(): ?string
+    {
+        return $this->nomp;
+    }
+
+    public function setNomp(string $nomp): static
+    {
+        $this->nomp = $nomp;
+
+        return $this;
+    }
+}
